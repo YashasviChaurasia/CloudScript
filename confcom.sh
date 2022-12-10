ccf5675483f48ced34b4e1f4efd6e097d2fee531
Yashasvi Chaurasia <46622381+YashasviChaurasia@users.noreply.github.com>
2022-12-10 23:47:10 +0530

    init confcom.sh

---
+++ confcom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo su
+yum update -y
+yum install git -y
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 15.0.0
+git clone https://github.com/YashasviChaurasia/Flexnet.git
+cd Flexnet
+npm install
+node index.js
+echo "Success"
