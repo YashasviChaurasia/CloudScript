3594aba22fc02fa107367d1be235dc9a17d5e613
Yashasvi Chaurasia <46622381+YashasviChaurasia@users.noreply.github.com>
2022-12-11 02:26:53 +0530

    Update confcom.sh

--- confcom.sh
+++ confcom.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user
+whoami>whoami.txt
+pwd>pwd.txt
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash 
 . ~/.nvm/nvm.sh
 nvm install 15.0.0
