bbfac2923bec90fc22a9ebe68d56269870180777
Yashasvi Chaurasia <46622381+YashasviChaurasia@users.noreply.github.com>
2022-12-11 00:26:48 +0530

    update v4

--- confcom.sh
+++ confcom.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
+echo "mf2">test3.log
 nvm install 15.0.0
 git clone https://github.com/YashasviChaurasia/Flexnet.git
 cd Flexnet
 npm install
+echo "npm_log">test4.log
 node index.js
 echo "Success"
 exit
