e1da4152437fa244b5a075307b186dc8a26ae648
Yashasvi Chaurasia <46622381+YashasviChaurasia@users.noreply.github.com>
2022-12-11 02:09:47 +0530

    Update confcom.sh

--- confcom.sh
+++ confcom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash /home/ec2-user /home/ec2-user/.nvm/nvm.sh
-echo "mf2">test3.log
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash 
+. ~/.nvm/nvm.sh
 nvm install 15.0.0
 git clone https://github.com/YashasviChaurasia/Flexnet.git
 cd Flexnet
